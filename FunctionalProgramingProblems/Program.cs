c893d4d5a4940a9f383fa35a829fade8935558e1
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-11 17:13:11 +0530

    [Darshan]: Add UC1-flip coin %

---
+++ FunctionalProgramingProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FunctionalProgramingProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose your selection from below");
+            Console.WriteLine("1. Flip Coin Percentage");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+                    FlipCoin uc1 = new FlipCoin();
+                    uc1.FlipCoinPercentage();
+                    Console.WriteLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Make Proper Selection");
+                    break;
+            }
+        }
+    }
+}

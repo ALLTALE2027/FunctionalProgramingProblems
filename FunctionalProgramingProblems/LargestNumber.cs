d6aff25f1341145ff21619e22971d8b729ebd980
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-11 19:03:26 +0530

    [Darshan]: Add UC 10 Largest Among Three Numbers

---
+++ FunctionalProgramingProblems/LargestNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramingProblems
+{
+    internal class LargestNumber
+    {
+        public void GetMaxNum()
+        {
+            Console.WriteLine("Program to Find the Largest Among Three Numbers");
+            Console.WriteLine("Enter the first Number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second Number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third Number");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Maximum from {0} {1} {2} is {3} ", a, b, c, a > b ? (a > c ? a : c) : (b > c ? b : c));
+        }
+    }
+}
--- FunctionalProgramingProblems/Program.cs
+++ FunctionalProgramingProblems/Program.cs
@@ -14,6 +14,7 @@
             Console.WriteLine("7. Swap Two Numbers");
             Console.WriteLine("8. Check Whether a Number is Even or Odd");
             Console.WriteLine("9. Alphabet is Vowel or Consonant");
+            Console.WriteLine("10. Find the Largest Among Three Numbers");
             int n = Convert.ToInt32(Console.ReadLine());
 
             switch (n)
@@ -50,6 +51,9 @@
                     break;
                 case 9:VowelConsonant check=new VowelConsonant();
                        check.CheckVowelConsonant();
+                    break;
+                case 10: LargestNumber largestNumber = new LargestNumber();
+                         largestNumber.GetMaxNum();
                     break;
 
 
